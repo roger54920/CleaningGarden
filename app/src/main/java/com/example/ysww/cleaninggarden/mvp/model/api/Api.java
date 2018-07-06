af73cd28e9ca8056e5dcefdda65006f574b39242
aini549200 <roger54920@163.com>
2018-07-06 11:01:58 +0800

    2018.7.6

---
+++ app/src/main/java/com/example/ysww/cleaninggarden/mvp/model/api/Api.java
@@ -0,0 +1,13 @@
+package com.example.ysww.cleaninggarden.mvp.model.api;
+
+import android.os.Build;
+/**
+ * 请求地址
+ */
+public class Api {
+    public static final int VERSION = Build.VERSION.SDK_INT; //android版本
+    public static String SOURCE_PAGE;//来源页面
+    public static final String LINK_HEAD="http://116.196.121.195:9999/trade-system/";//测试链接头
+//    public static final String LINK_HEAD="http://daitaowx.mynatapp.cc/trade-system/";//本地链接头
+
+}
