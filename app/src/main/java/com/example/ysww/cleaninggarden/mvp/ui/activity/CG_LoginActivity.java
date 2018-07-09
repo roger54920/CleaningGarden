88a01fc3343cf522f6f9cf0360ef235dea7eb952
aini549200 <roger54920@163.com>
2018-07-09 15:21:15 +0800

    2018.07.09

--- app/src/main/java/com/example/ysww/cleaninggarden/mvp/ui/activity/CG_LoginActivity.java
+++ app/src/main/java/com/example/ysww/cleaninggarden/mvp/ui/activity/CG_LoginActivity.java
@@ -1,55 +1,31 @@
 package com.example.ysww.cleaninggarden.mvp.ui.activity;
 
-import android.content.Context;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-import com.baidu.location.BDAbstractLocationListener;
-import com.baidu.location.BDLocation;
-import com.baidu.location.LocationClient;
-import com.baidu.location.LocationClientOption;
 import com.example.ysww.cleaninggarden.R;
-import com.example.ysww.cleaninggarden.app.Logger;
+import com.example.ysww.cleaninggarden.app.utils.BDLocationUtils;
 
 
 public class CG_LoginActivity extends AppCompatActivity {
-    public LocationClient mLocationClient = null;
-    private MyLocationListener myListener = new MyLocationListener();
-
+    private BDLocationUtils bdLocationUtils;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cg__login);
-        mLocationClient = new LocationClient(getApplicationContext());
-        //声明LocationClient类
-        mLocationClient.registerLocationListener(myListener);
-        //注册监听函数
-        LocationClientOption option = new LocationClientOption();
-
-        option.setIsNeedAddress(true);
-//可选，是否需要地址信息，默认为不需要，即参数为false
-//如果开发者需要获得当前点的地址信息，此处必须为true
-
-        mLocationClient.setLocOption(option);
+        bdLocationUtils = new BDLocationUtils(this);
     }
-    public class MyLocationListener extends BDAbstractLocationListener {
-        @Override
-        public void onReceiveLocation(BDLocation location){
-            //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
-            //以下只列举部分获取地址相关的结果信息
-            //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
-
-            String addr = location.getAddrStr();    //获取详细地址信息
-            String country = location.getCountry();    //获取国家
-            String province = location.getProvince();    //获取省份
-            String city = location.getCity();    //获取城市
-            String district = location.getDistrict();    //获取区县
-            String street = location.getStreet();    //获取街道信息
-            Logger.d("百度地图",addr+" "+country+" "+province+" "+city+" "+district+" "+street);
-        }
+    @Override
+    protected void onStop() {
+        // TODO Auto-generated method stub
+        bdLocationUtils.stopLocation();
+        super.onStop();
     }
 
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+        bdLocationUtils.startLocation();//开启定位
+    }
 }
